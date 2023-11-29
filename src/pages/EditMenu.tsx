d8f49d5e1f41894802923a2a0e4065e14154cde7
lanbow93 <the.doctor1501@gmail.com>
2023-11-29 14:28:47 -0600

    can add in items to list now

--- src/pages/EditMenu.tsx
+++ src/pages/EditMenu.tsx
@@ -16,85 +16,78 @@ interface InstrumentData {
   }
 
   
-function EditMenu(){
-    const [isLoading, setIsLoading] = useState(false)
-    const [isModalActive, setIsModalActive] = useState(false)
+  function EditMenu() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [isModalActive, setIsModalActive] = useState(false);
     const [errorData, setErrorData] = useState({
-        errorMessage: '',
-        errorStatus: '',
-        errorAdditional: '',
-    })
-
+      errorMessage: '',
+      errorStatus: '',
+      errorAdditional: '',
+    });
+  
     const [instrumentData, setInstrumentData] = useState<InstrumentData>({
-        name: "",
-        description: "",
-        image: null,
-        price: 0,
-        quantityAvailable: 0,
-        brand: "",
-        category: "",
-        condition: ""
-    })
+      name: "",
+      description: "",
+      image: null,
+      price: 0,
+      quantityAvailable: 0,
+      brand: "",
+      category: "",
+      condition: ""
+    });
+  
+    const navigate = useNavigate(); // Moved to the top
+  
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        // Handle file change and update the state
-        const files = event.target.files;
-    
-        if (files && files.length > 0) {
-          const file = files[0];
-          setInstrumentData((prevData) => ({ ...prevData, image: file }));
-        }
-      };
-    
-    const handleSubmission = async (
-        event: React.FormEvent<HTMLFormElement>
-    ) => {
-        event.preventDefault()
-        setIsLoading(true)
-        try {
-            const formData = new FormData();
-            formData.append('name', instrumentData.name);
-            formData.append('description', instrumentData.description);
-            formData.append('image', instrumentData.image); // Append the file
-            formData.append('price', instrumentData.price);
-            formData.append('quantityAvailable', instrumentData.quantityAvailable);
-            formData.append('brand', instrumentData.brand);
-            formData.append('category', instrumentData.category);
-            formData.append('condition', instrumentData.condition);
-      
-            const response = await fetch(url + '/instruments', {
-              method: 'POST',
-              body: formData,
-            });
-      
-            if (response.ok) {
-              navigate('/editmenu');
-            } else {
-              const data = await response.json();
-              const { error, message, status } = data;
-              setErrorData({
-                errorMessage: message,
-                errorAdditional: status,
-                errorStatus: error,
-              });
-              setIsModalActive(true);
-            }
-          } catch (error) {
-            console.error('Error during submission:', error);
-          } finally {
-            setIsLoading(false);
-          }
-        };
-
+      const files = event.target.files;
+  
+      if (files && files.length > 0) {
+        const file = files[0];
+        setInstrumentData((prevData) => ({ ...prevData, image: file }));
+      }
+    };
+  
     const handleInputChange = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>) => {
-        const { name, value } = event.target
-        // Use the validateNoSpaces function for the username field
-
-        setInstrumentData({ ...instrumentData, [name]: value })
-    }
-    const navigate = useNavigate()
-
-
-
+      const { name, value } = event.target;
+      setInstrumentData({ ...instrumentData, [name]: value });
+    };
+  
+    const handleSubmission = async (event: React.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      setIsLoading(true);
+  
+      const formData = new FormData();
+  
+      // Append form data
+      Object.entries(instrumentData).forEach(([key, value]) => {
+        formData.append(key, value);
+      });
+  
+      try {
+        const response = await fetch(url + '/instruments', {
+          method: 'POST',
+          credentials: 'include',
+          body: formData,
+        });
+  
+        if (response.ok) {
+          navigate('/editmenu');
+        } else {
+          const data = await response.json();
+          const { error, message, status } = data;
+          setErrorData({
+            errorMessage: message,
+            errorAdditional: status,
+            errorStatus: error,
+          });
+          setIsModalActive(true);
+        }
+      } catch (error) {
+        console.error('Error during submission:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
 
     return (
@@ -115,7 +108,7 @@ function EditMenu(){
                 {isLoading ? (
                     <LoadingScreen />
                 ) : (
-                    <form onSubmit={handleSubmission}>
+                    <form onSubmit={handleSubmission} encType="multipart/form-data">
     <h2>Instrument Details</h2>
     
     <label>Name:</label>
