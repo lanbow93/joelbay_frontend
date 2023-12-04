7700e848b511b848c6da1b2daa110251917ef4b5
lanbow93 <the.doctor1501@gmail.com>
2023-12-04 15:50:58 -0600

    fixed import on router page for CreateMenu

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,7 +6,7 @@ import {
 import App from '../App'
 import Homepage from '../pages/Homepage'
 import AdminLogin from '../pages/AdminLogin'
-import EditMenu from '../pages/EditMenu'
+import CreateMenu from '../pages/EditMenu'
 import Listings from '../pages/Listings'
 
 const router = createBrowserRouter(
@@ -14,7 +14,7 @@ const router = createBrowserRouter(
         <Route path="/" element={<App />}>
             <Route path="/" element={<Homepage />} />
             <Route path="/sitemaster" element={<AdminLogin />} />
-            <Route path="/editmenu" element={<EditMenu />} />
+            <Route path="/editmenu" element={<CreateMenu />} />
             <Route path="/listings" element={<Listings />} />
         </Route>
     )
