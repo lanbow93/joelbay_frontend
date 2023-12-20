31d35ba6ffcfc35fff37a42da2060071497ee76b
lanbow93 <the.doctor1501@gmail.com>
2023-12-20 09:46:51 -0600

    able to filter listings

--- src/pages/Listings.tsx
+++ src/pages/Listings.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import  { useState, useEffect } from 'react'
 import Loading from '../components/Loading'
 import ErrorScreen from '../components/ErrorScreen'
 import ListingCard from '../components/ListingCard'
@@ -14,15 +14,26 @@ function Listings() {
         errorAdditional: '',
     })
     const [listingData, setListingData] = useState([])
+    type SelectedFilters = {
+        category: string[];
+        brand: string[];
+        condition: string[];
+    };
+
+    const [selectedFilters, setSelectedFilters] = useState<SelectedFilters>({
+        category: [] as string[],
+        brand: [] as string[],
+        condition: [] as string[]
+    })
+
     useEffect(() => {
         getListings()
-        return () => {}
     }, [])
 
     const getListings = async () => {
         setIsLoading(true)
-        const response: any = await listingCall()
-        
+        const response = await listingCall()
+
         setIsLoading(false)
         if (response.data) {
             setListingData(response.data)
@@ -35,12 +46,49 @@ function Listings() {
             })
             setIsModalActive(true)
         }
+
     }
 
+    
+
+    const handleCheckboxChange = (filterType: any, option: any) => {
+        const updatedFilters: any = { ...selectedFilters }
+        
+        // Toggle the option in the selectedFilters state
+        const index = updatedFilters[filterType].indexOf(option)
+        if (index === -1) {
+            updatedFilters[filterType].push(option)
+        } else {
+            updatedFilters[filterType].splice(index, 1)
+        }
+
+        // Update the state with the new selectedFilters
+        setSelectedFilters(updatedFilters)
+    }
+    
+    if(selectedFilters.category.length === 0 &&
+        selectedFilters.brand.length === 0 &&
+        selectedFilters.condition.length === 0){
+        setSelectedFilters({
+            category: uniqueObjectArrayList(listingData, 'category'),
+            brand: uniqueObjectArrayList(listingData, 'brand'),
+            condition: uniqueObjectArrayList(listingData, 'condition'),
+        });
+    }
     if (isLoading) {
         return <Loading />
     }
-    console.log(uniqueObjectArrayList(listingData, "category"))
+
+    const filteredListings = listingData.filter((listing: any) => {
+        // Check if the listing matches the selected filters
+        const categoryFilter = selectedFilters.category.length === 0 || selectedFilters.category.includes(listing.category);
+        const brandFilter = selectedFilters.brand.length === 0 || selectedFilters.brand.includes(listing.brand);
+        const conditionFilter = selectedFilters.condition.length === 0 || selectedFilters.condition.includes(listing.condition);
+    
+        // Return true if all filters pass
+        return categoryFilter && brandFilter && conditionFilter;
+    });
+
     return (
         <div className="listings">
             <div className={`errorModal ${isModalActive ? 'showError' : ''}`}>
@@ -54,15 +102,60 @@ function Listings() {
             <h1>Listings Page</h1>
             <div className="filter">
                 <h2>Filter Options</h2>
-                <p className='bold underline'>Category</p>
-                <ul className="filterOptions">{uniqueObjectArrayList(listingData, "category").map((option: string) => <li><input type="checkbox" name={option} id={option}/><label htmlFor={option}>{option}</label></ li>)}</ul>
-                <p className='bold underline'>Brand</p>
-                <ul className="filterOptions">{uniqueObjectArrayList(listingData, "brand").map((option: string) => <li><input type="checkbox" name={option} id={option}/><label htmlFor={option}>{option}</label></ li>)}</ul>
-                <p className='bold underline'>Condition</p>
-                <ul className="filterOptions">{uniqueObjectArrayList(listingData, "condition").map((option: string) => <li><input type="checkbox" name={option} id={option}/><label htmlFor={option}>{option}</label></ li>)}</ul>
+                <p className="bold underline">Category</p>
+                <ul className="filterOptions">
+                    {uniqueObjectArrayList(listingData, 'category').map(
+                        (option: string) => (
+                            <li key={option}>
+                                <input
+                                    type="checkbox"
+                                    name={option}
+                                    id={option}
+                                    checked={selectedFilters.category.includes(option)}
+                                    onChange={() => handleCheckboxChange('category', option)}
+                                />
+                                <label htmlFor={option}>{option}</label>
+                            </li>
+                        )
+                    )}
+                </ul>
+                <p className="bold underline">Brand</p>
+                <ul className="filterOptions">
+                    {uniqueObjectArrayList(listingData, 'brand').map(
+                        (option: string) => (
+                            <li key={option}>
+                                <input
+                                    type="checkbox"
+                                    name={option}
+                                    id={option}
+                                    checked={selectedFilters.brand.includes(option)}
+                                    onChange={() => handleCheckboxChange('brand', option)}
+                                />
+                                <label htmlFor={option}>{option}</label>
+                            </li>
+                        )
+                    )}
+                </ul>
+                <p className="bold underline">Condition</p>
+                <ul className="filterOptions">
+                    {uniqueObjectArrayList(listingData, 'condition').map(
+                        (option: string) => (
+                            <li key={option}>
+                                <input
+                                    type="checkbox"
+                                    name={option}
+                                    id={option}
+                                    checked={selectedFilters.condition.includes(option)}
+                                    onChange={() => handleCheckboxChange('condition', option)}
+                                />
+                                <label htmlFor={option}>{option}</label>
+                            </li>
+                        )
+                    )}
+                </ul>
             </div>
             <div className="listingDisplay">
-                {listingData.map((listing: any) => (
+                {filteredListings.map((listing: any) => (
                     <ListingCard {...listing} key={listing.id + listing.name} />
                 ))}
             </div>
--- src/utils/SharedFunctions.tsx
+++ src/utils/SharedFunctions.tsx
@@ -9,15 +9,18 @@ export function dateConverter(date: string) {
 }
 
 type listingObject = {
-    brand: string,
-    category: string,
-    condition: string,
+    brand: string
+    category: string
+    condition: string
 }
-export function uniqueObjectArrayList(objectArrayToCheck: Array<listingObject>, keyToCheck: "brand" | "category" | "condition"){
-    let arrayOfUniques: string[] = [];
-    for(let i=0; i<objectArrayToCheck.length; i++){
-        if(!arrayOfUniques.includes(objectArrayToCheck[i][keyToCheck]) ){
-            arrayOfUniques.push(objectArrayToCheck[i][keyToCheck]);
+export function uniqueObjectArrayList(
+    objectArrayToCheck: Array<listingObject>,
+    keyToCheck: 'brand' | 'category' | 'condition'
+) {
+    let arrayOfUniques: string[] = []
+    for (let i = 0; i < objectArrayToCheck.length; i++) {
+        if (!arrayOfUniques.includes(objectArrayToCheck[i][keyToCheck])) {
+            arrayOfUniques.push(objectArrayToCheck[i][keyToCheck])
         }
     }
     return arrayOfUniques
